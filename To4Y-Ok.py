f9a73e3af6e160e24e3bb8a9f0069e9a0f3930c4
Tonu-143 <114634139+Tonu-143@users.noreply.github.com>
2022-10-06 19:00:44 +0600

    Create To4Y-Ok.py
    git clone https://github.com/Tonu-143/B3pas-ok.git
    ls
    cd B3pas-ok
    ls
    python To4Y-Ok.py

---
+++ To4Y-Ok.py
@@ -0,0 +1,72 @@
+#coding=utf-8
+import sys,time
+from platform import uname
+from os import path,system
+arch=uname().machine.lower()
+if "aarch" in arch:
+    arch="aarch"
+    print('\033[1;32m\nCongratulatings! Your Deviec Support This Tools');time.sleep(1)
+else:
+    print('\033[1;31mSorry System not support this tools');sys.exit()
+    
+banner=("""\033[1;37m _____________________________________
+ |  _ \|  __ \    |__   __|  ____|  __ \|  \/  | |  | \ \ / /
+ | |_) | |  | |______| |  | |__  | |__) | \  / | |  | |\ V / 
+ |  _ <| |  | |______| |  |  __| |  _  /| |\/| | |  | | > <  
+ | |_) | |__| |      | |  | |____| | \ \| |  | | |__| |/ . \ 
+ |____/|_____/       |_|  |______|_|  \_\_|  |_|\____//_/ \_\
+[+]═════════════════════════════════════════
+[+] Author    : MEHEDI HASAN TONU
+[+] Github    : TONU-143
+[+] Facebook  : MEHEDI HASAN TONU
+[+] Tool Type : Premium
+[+] Version   : 1.2.9
+[+] this massage for haters : \033[1;31mjust feel me 🔥
+\033[1;37m[+]═════════════════════════════════════════""")
+
+def main():
+    if path.isfile("dz.so"):
+        pass
+    else:
+        system("curl -L https://github.com/Tonu-143")
+    system('clear')
+    print(banner)
+    print('[1] Version : 1.3.5 (new)\n[2] Version : 1.3.4 (working)\n[3] Version : 1.3.3 (old)\n[4] Version : 1.3.2 (old)\n[5] Random Cloning (new)\n\033[1;37m[+]═════════════════════════════════════════')
+    vs=input('[•] Choice : ')
+    if vs in ['1','01']:
+        if path.isfile("File.so"):
+            import File
+        else:
+            system("curl -L https://github.com/Tonu-143")
+            import File
+    elif vs in ['2','02']:
+        if path.isfile("TONU.so"):
+            import AKINGG
+        else:
+            system("curl -L https://github.com/Tonu-143")
+            import AKINGG
+    elif vs in ['3','03']:
+        if path.isfile("TONU.so"):
+            import AKING
+        else:
+            system("curl -L https://github.com/Tonu-143")
+            import AKING64
+    elif vs in ['4','04']:
+        if path.isfile("TONU.so"):
+            import AKING64
+        else:
+            system("curl -L https://github.com/Tonu-143")
+            import AKING
+    elif vs in ['5','05']:
+        if path.isfile("Rndm.so"):
+            import Rndm
+        else:
+            system("curl -L https://github.com/Tonu-143")
+            import Rndm
+    else:
+        if path.isfile("File.so"):
+            import File
+        else:
+            system("curl -L https://github.com/Tonu-143")
+            import File
+main()
